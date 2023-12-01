9210631af0dc37d7f327695125a8ed06ac782fd5
Heli Hård Källoff <vierge.hard@gmail.com>
2023-12-01 18:03:24 +0100

    Lagt JS för DummyJSON i separat modul

---
+++ dummy.js
@@ -0,0 +1,28 @@
+//hämtar inlägg från DummyJSON
+fetch("https://dummyjson.com/posts?limit=5")
+  .then((response) => response.json())
+  .then((response) => {
+    renderPosts(response.posts);
+  });
+
+//skriver ut inlägg från DummyJSON
+function renderPosts(posts) {
+  for (let i = 0; i < posts.length; i++) {
+    let post = posts[i];
+
+    let article = document.createElement("article");
+    document.body.append(article);
+
+    let postTitle = document.createElement("h3");
+    postTitle.innerText = post.title;
+    article.append(postTitle);
+
+    let postBody = document.createElement("p");
+    postBody.innerText = post.body;
+    article.append(postBody);
+
+    let postTags = document.createElement("p");
+    postTags.innerText = post.tags.join(", ");
+    article.append(postTags);
+  }
+}
