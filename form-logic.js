48cceba0710f4e218ff9c89b0d6d0c3ddbc24855
Heli Hård Källoff <vierge.hard@gmail.com>
2023-12-06 19:45:23 +0100

    Lagt alla funktioner som har att göra med formulärets utseende/beteende i egen module

---
+++ form-logic.js
@@ -0,0 +1,55 @@
+//logik för formuläret
+
+//skapar åtkomst till element i html-filen, de som behövs i script-filen exporteras
+export const newPostTitle = document.querySelector("#new-title");
+export const newPostBody = document.querySelector("#new-post");
+export const newPostTags = document.querySelectorAll(".new-tag");
+const newPostTagsDiv = document.querySelector("#new-tags-div");
+const submitBtn = document.querySelector("#submit-new-post");
+
+export function formInactive() {
+  newPostBody.style.height = "auto";
+  newPostTitle.style.display = "none";
+  newPostTagsDiv.style.display = "none";
+  submitBtn.style.display = "none";
+  submitBtn.disabled = true;
+}
+
+formInactive();
+
+//visar hela formuläret vid fokus i textarea
+newPostBody.addEventListener("focus", () => {
+  newPostBody.style.height = "200px";
+  newPostTitle.style.display = "block";
+  newPostTagsDiv.style.display = "block";
+  submitBtn.style.display = "block";
+
+  //kollar om det finns text i textarea
+  newPostBody.addEventListener("input", checkIfEmpty);
+});
+
+function checkIfEmpty() {
+  let fieldToCheck = this.value;
+
+  //tar bort whitespace i början och slutet av texten
+  let trimmedNewBodyValue = fieldToCheck.trim();
+
+  //om textarea inte är tom och om det inmatade inte enbart är whitespaces: visa submitknappen
+  //(endast text i textarea är obligatoriskt för att kunna posta)
+  if (fieldToCheck !== "" && trimmedNewBodyValue.length !== 0) {
+    submitBtn.disabled = false;     
+  } else if (trimmedNewBodyValue.length === 0){
+    submitBtn.disabled = true;
+  }
+}
+
+//begränsar antal taggar för nytt inlägg
+(function() {
+  newPostTags.forEach((el) => {
+    el.addEventListener("change", function() {
+      if (el.closest("#new-tags-div").querySelectorAll("input:checked").length > 3) {
+        this.checked = false;
+      }
+    });
+  });
+})();
