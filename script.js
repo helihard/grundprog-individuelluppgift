3c9c00d852e4073eea3bf93cec020c71925f2cbf
Heli Hård Källoff <vierge.hard@gmail.com>
2023-12-06 14:45:30 +0100

    Börjat om script för formuläret med tillämpning av localStorage – manipulation av element och funktioner för bearbetning av data ska byggas in igen från tidigare kod

--- script.js
+++ script.js
@@ -1,146 +1,49 @@
+//skapar åtkomst till element i html-filen
 const postForm = document.querySelector("#create-new-post");
 const newPostTitle = document.querySelector("#new-title");
-const newPostInput = document.querySelector("#new-post");
-const newPostTagsDiv = document.querySelector("#new-tags-div");
-const newPostTags = document.querySelectorAll(".new-tag");
-const submitBtn = document.querySelector("#submit-new-post");
+const newPostBody = document.querySelector("#new-post");
 const newPostsDiv = document.querySelector("#new-posts-div");
+const dummyPostsDiv = document.querySelector("#dummy-posts-div");
 
+//skapar en klass för nya inlägg
 class Post {
-  constructor (title, body, tags) {
+  constructor(title, body) {
     this.title = title;
     this.body = body;
-    this.tags = tags;
   }
 }
 
-submitBtn.disabled = true;
-newPostTitle.style.display ="none";
-newPostTagsDiv.style.display = "none";
-submitBtn.style.display = "none";
-
-//visar hela formuläret vid focus i textarea
-newPostInput.addEventListener("focus", () => {
-  newPostInput.style.height = "200px";
-  newPostTitle.style.display = "block";
-  newPostTagsDiv.style.display = "block";
-  submitBtn.style.display = "block";
-
-  //kollar om det finns text i textarea
-  newPostInput.addEventListener("input", checkIfEmpty);
-});
-
-function checkIfEmpty() {
-  let fieldToCheck = this.value;
-
-  //tar bort whitespace i början och slutet av texten
-  let trimmedNewBodyValue = fieldToCheck.trim();
-
-  //om textarea inte är tom och om det inmatade inte enbart är whitespaces: visa submitknappen
-  //(endast text i textarea är obligatoriskt för att kunna posta)
-  if (fieldToCheck !== "" && trimmedNewBodyValue.length !== 0) {
-    submitBtn.disabled = false;     
-  } else if (trimmedNewBodyValue.length === 0){
-    submitBtn.disabled = true;
-  }
-}
-
-//begränsar antal taggar för nytt inlägg
-(function() {
-  newPostTags.forEach(el => {
-    el.addEventListener("change", function() {
-      if (el.closest("#new-tags-div").querySelectorAll("input:checked").length > 3) {
-        this.checked = false;
-      }
-    });
-  });
-})();
-
-//hämta data från formulär på submit
-postForm.addEventListener("submit", getFormData);
-
-//hämtar datan
-function getFormData(event) {
-  event.preventDefault();
-
-  //sammanställ nytt inlägg från hämtad data
-  renderNewPost(compileNewPost(postForm));
-
-  //återställ formulär efter submit: töm, dölj delar av formulär, disabla submitknapp 
-  postForm.reset();
-  newPostInput.style.height = "auto";
-  newPostTitle.style.display = "none";
-  newPostTagsDiv.style.display = "none";
-  submitBtn.style.display = "none";
-  submitBtn.disabled = true;
-}
-
-//sammanställer hämtad data från formuläret till ett inlägg
-function compileNewPost(form) {
-  let checkedTags = [];
-
-  //valda taggar läggs i en array
-  for (let tag of newPostTags) {
-    if (tag.checked === true) { // && checkedTags.length < 3
-      checkedTags.push(tag.value);
-    }
-  }
-
-  let newPost = new Post(form.newtitle.value, form.newbody.value, checkedTags);
-
-  //tar bort whitespaces i början och slutet av inläggstexten och titeln
-  newPost.body = newPost.body.trim();
-  newPost.title = newPost.title.trim();
-
-  //autogenerera titel från inläggstexten om användaren inte anger titel
-  if (newPost.title === "") {
-    newPost.title = renderDefaultTitle(newPost.body);
-  } else {
-    newPost.title;
-  }
-  return newPost;
-}
-
-//autogenererar titel
-function renderDefaultTitle(body) {
-  //delar upp inlägget i rader
-  //alla tomma rader blir en tom sträng i arrayen även om de innehåller mellanslag
-  const splitByNewLine = body.split(/\r?\n/).map(line => line.trim());
-  //console.log(splitByNewLine);
-  //första raden i inlägget
-  const firstLine = splitByNewLine[0];
- 
-  //delar upp första raden i ord
-  const splitByWhiteSpace = firstLine.split(" ");
-  //console.log(splitByWhiteSpace);
-  const defaultTitleArray = [];
-
-  //lägger bestämt maxantal ord i arrayen som ska bli inläggets genererade titel
-  for (let i = 0; i < splitByWhiteSpace.length; i++) {
-    let word = splitByWhiteSpace[i];
-
-    //max sex ord i titeln
-    if (i < 6) {
-      defaultTitleArray.push(word);
-    }
-  }
-
-  //skapar en string av arrayen: detta är titeln
-  let defaultTitle = defaultTitleArray.join(" ");
-
-  //returnerar titeln
-  return defaultTitle;
-}
-
-//skriver ut data i form av nytt inlägg
-function renderNewPost(post) {
+//skriver ut nytt inlägg
+function printNewPost(post) {
   let newArticle = document.createElement("article");
   let newTitle = document.createElement("h3");
-  let newPostBody = document.createElement("p");
-  let newTags = document.createElement("p");
-  newTitle.innerText = post.title;
-  newPostBody.innerText = post.body;
-  newTags.innerText = post.tags.join(", ");
-  newArticle.append(newTitle, newPostBody, newTags);
+  let newBody = document.createElement("p");
+  newTitle.textContent = post.title;
+  newBody.textContent = post.body;
+  newArticle.append(newTitle, newBody);
   newPostsDiv.append(newArticle);
+}
+
+//när sidan laddas: 
+//finns arrayen newPostsArray redan i localStorage? 
+//om ja – parse
+//om nej – skapa newPostsArray som en tom array
+let newPostsArray = localStorage.getItem("newPosts") ? JSON.parse(localStorage.getItem("newPosts")) : [];
+//för varje item som finns i newPostsArray, skriv ut
+newPostsArray.forEach(printNewPost);
+
+//om submit – läs in data från formuläret
+postForm.addEventListener("submit", getFormData);
+
+//läser in data från formuläret
+function getFormData(event) {
+  event.preventDefault();
+  let title = newPostTitle.value;
+  let body = newPostBody.value;
+  let newPost = new Post(title, body);
+  newPostsArray.push(newPost);
+  localStorage.setItem("newPosts", JSON.stringify(newPostsArray));
+  //skriv ut nytt inlägg
+  printNewPost(newPost);
+  postForm.reset();
 }
