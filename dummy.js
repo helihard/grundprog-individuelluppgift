c5d2e5fd1520b91ff8f137e26bdd866e787650a6
Heli Hård Källoff <vierge.hard@gmail.com>
2023-12-06 14:24:04 +0100

    Skrivit om funktionerna för dummyinlägg: fetch till async IIFE; funktionen som skriver ut dummy-inlägg lägger nu dessa på rätt ställe (egen div) och loopar igenom varje inläggs taggar och lägger dessa i separata element

--- dummy.js
+++ dummy.js
@@ -1,27 +1,34 @@
 //hämtar inlägg från DummyJSON
-fetch("https://dummyjson.com/posts?limit=5")
-  .then((response) => response.json())
-  .then((response) => {
-    renderPosts(response.posts);
-  });
+//sparar inlägg i localStorage - får se om det går att göra något med detta
+(async () => {
+  const response = await fetch("https://dummyjson.com/posts?limit=5");
+  const dummyData = await response.json();
+  localStorage.setItem("dummyPosts", JSON.stringify(dummyData.posts));
+  printDummyData(dummyData.posts);
+})();
 
 //skriver ut inlägg från DummyJSON
-function renderPosts(posts) {
-  for (let i = 0; i < posts.length; i++) {
-    let post = posts[i];
+//se om det går att göra en gemensam printfunktion för nya och gamla inlägg
+function printDummyData(posts) {
+  posts.forEach((post) => {
+  
+    const article = document.createElement("article");
+    const title = document.createElement("h3");
+    const body = document.createElement("p");
+    const tags = document.createElement("p");
+    tags.classList.add("tag-div");
 
-    let article = document.createElement("article");
-    document.body.append(article);
+    post.tags.forEach((tag) => {
+      const tagSpan = document.createElement("span");
+      tagSpan.classList.add("tag-span");
+      tagSpan.textContent = tag;
+      tags.append(tagSpan);
+    });
 
-    let postTitle = document.createElement("h3");
-    postTitle.innerText = post.title;
+    title.textContent = post.title;
+    body.textContent = post.body;
 
-    let postBody = document.createElement("p");
-    postBody.innerText = post.body;
-
-    let postTags = document.createElement("p");
-    postTags.innerText = post.tags.join(", ");
-
-    article.append(postTitle, postBody, postTags);
-  }
+    article.append(title, body, tags);
+    dummyPostsDiv.append(article);
+  });
 }
