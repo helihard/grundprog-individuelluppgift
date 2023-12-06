9f3285e0770612555d842489a4d8cad1f0ad15f8
Heli Hård Källoff <vierge.hard@gmail.com>
2023-12-06 17:31:57 +0100

    Fixat Post-klassen, funktionen som hämtar data från formuläret och funktionen som skriver ut nytt inlägg så att tags nu är med överallt

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+import { newPostTitle, newPostBody, newPostTags, formInactive } from "./form-logic.js";
+
 //skapar åtkomst till element i html-filen
 const postForm = document.querySelector("#create-new-post");
-const newPostTitle = document.querySelector("#new-title");
-const newPostBody = document.querySelector("#new-post");
 const newPostsDiv = document.querySelector("#new-posts-div");
-const dummyPostsDiv = document.querySelector("#dummy-posts-div");
 
 //skapar en klass för nya inlägg
 class Post {
-  constructor(title, body) {
+  constructor(title, body, tags) {
     this.title = title;
     this.body = body;
+    this.tags = tags;
   }
 }
 
 //skriver ut nytt inlägg
 function printNewPost(post) {
-  let newArticle = document.createElement("article");
-  let newTitle = document.createElement("h3");
-  let newBody = document.createElement("p");
+  const newArticle = document.createElement("article");
+  const newTitle = document.createElement("h3");
+  const newBody = document.createElement("p");
+  const newTags = document.createElement("p");
+  newTags.classList.add("tag-div");
+
+  post.tags.forEach((tag) => {
+    const newTagSpan = document.createElement("span");
+    newTagSpan.classList.add("tag-span");
+    newTagSpan.textContent = tag;
+    newTags.append(newTagSpan);
+  });
+
   newTitle.textContent = post.title;
   newBody.textContent = post.body;
-  newArticle.append(newTitle, newBody);
+  
+  newArticle.append(newTitle, newBody, newTags);
   newPostsDiv.append(newArticle);
 }
 
@@ -38,12 +49,23 @@ postForm.addEventListener("submit", getFormData);
 //läser in data från formuläret
 function getFormData(event) {
   event.preventDefault();
+
   let title = newPostTitle.value;
   let body = newPostBody.value;
-  let newPost = new Post(title, body);
+  let checkedTags = [];
+
+  //valda taggar läggs i en array
+  for (let tag of newPostTags) {
+    if (tag.checked === true) { // && checkedTags.length < 3
+      checkedTags.push(tag.value);
+    }
+  }
+
+  let newPost = new Post(title, body, checkedTags);
   newPostsArray.push(newPost);
   localStorage.setItem("newPosts", JSON.stringify(newPostsArray));
   //skriv ut nytt inlägg
   printNewPost(newPost);
   postForm.reset();
+  formInactive();
 }
